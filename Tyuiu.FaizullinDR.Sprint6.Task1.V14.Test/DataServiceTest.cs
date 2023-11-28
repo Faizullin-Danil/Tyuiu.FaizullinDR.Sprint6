3b09255f36f3390d9cdccb17a2ccb5ffb6468289
TyuiuFaizullinDR@outlook.com <toxen2005@mail.ru>
2023-11-28 12:20:24 +0500

    Task1

--- Tyuiu.FaizullinDR.Sprint6.Task1.V14.Test/DataServiceTest.cs
+++ Tyuiu.FaizullinDR.Sprint6.Task1.V14.Test/DataServiceTest.cs
@@ -10,6 +10,10 @@ namespace Tyuiu.FaizullinDR.Sprint6.Task1.V14.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+            double[] wait = { -2.15, -2.57, -3.00, -3.83, -11.70, 3.00, 2.19, 3.31, 2.97, 1.18, 0.03 };
+            double[] res = ds.GetMassFunction(-5, 5);
+            CollectionAssert.AreEqual(wait, res);
         }
     }
 }
